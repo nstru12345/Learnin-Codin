f79bec41a2053743a21bc567ffd89083853d5ffd
nstru12345 <102781560+nstru12345@users.noreply.github.com>
2022-08-16 18:12:14 -0700

    Made a calculator in C#!
    My first program, it is very simple but I made it off of memory + Microsoft documentation so I'm very proud!

---
+++ Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MyNameSpace
+{
+    class practice
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your first number");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter your second number");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Multiply(*) Divide(/) Add(+) or Subtract?(-)");
+            string theOperation = Console.ReadLine();
+
+            while (theOperation != "r")
+            {
+                if (theOperation == "*")
+                {
+                    Console.WriteLine(a * b);
+                    Main();
+                }
+                else if (theOperation == "/")
+                {
+                    Console.WriteLine(a / b);
+                    Main();
+                }
+                else if (theOperation == "+")
+                {
+                    Console.WriteLine(a + b);
+                    Main();
+                }
+                else if (theOperation == "-")
+                {
+                    Console.WriteLine(a - b);
+                    Main();
+                }
+            }
+        }
+    }
+}
